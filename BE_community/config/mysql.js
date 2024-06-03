8b0d0abbe8423cbe34a5fc5981f7ba89c4c8d12e
Elle <elle@Elleui-MacBookPro.local>
2024-06-03 17:38:44 +0900

    docs : env 적용

--- BE_community/config/mysql.js
+++ BE_community/config/mysql.js
@@ -1,11 +1,14 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db_info = mysql.createPool({
-    host: "localhost", // 데이터베이스 주소
-    port: "3306", // 데이터베이스 포트
-    user: "yeonsu", // 로그인 계정
-    password: "2366", // 비밀번호
-    database: "community", // 엑세스할 데이터베이스
+    host: process.env.DB_HOST, // 데이터베이스 주소
+    port: process.env.DB_PORT, // 데이터베이스 포트
+    user: process.env.DB_USER, // 로그인 계정
+    password: process.env.DB_PASSWORD, // 비밀번호
+    database: process.env.DB_DATABASE, // 엑세스할 데이터베이스
 });
 
 db_info.getConnection()
